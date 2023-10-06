1ae434073d91c53e3d72f932c42b6b94ce5e3a32
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-06 23:44:00 +0300

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,11 +16,11 @@ int main(void)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
-				if (i != j || j != k || i != k)
+				if (i != j && j != k && i != k)
 				{
-					putchar(i);
-					putchar(j);
-					putchar(k);
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
 					if (i != 7 || j != 8 || k != 9)
 					{
 						putchar(',');
