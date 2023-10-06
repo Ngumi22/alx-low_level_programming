31069ac524349cb195dc134c47cd6dddc2054072
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-06 23:14:10 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,16 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			if (i != 0 || j != 0)
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 9 || j != 9)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 9 || j != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
