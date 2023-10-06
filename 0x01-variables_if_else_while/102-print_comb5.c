e77ea93d8ec85d6d6094fc3d3c0dca016ec3ba9c
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-07 00:03:30 +0300

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,17 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int n;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i; j <= 9; j++)
 		{
-			for (k = 0; k <= 9; k++)
+			putchar(i + '0');
+			putchar(j + '0');
+			if (!(i == 9 && j == 9))
 			{
-				for (n = 0; n <= 9; n++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(n + '0');
-					if (i != 9 || j != 9 || k != 9 || n != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
