d51737e5b1a35d003b0fdde9a03da730584a4014
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-06 23:06:47 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (int i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (int j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (i != 0 || j != 0)
 			{
