7b047200f69f889eddad5c0af00f65a0a26edcf6
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-11 22:12:36 +0300

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,21 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  * Return: Every minute of Jack Bauer
  */
-int main(void)
+void jack_bauer(void)
 {
-	jack_bauer();
-	return (0);
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
