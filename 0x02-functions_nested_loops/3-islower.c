673ac04c8f7dcbb9ed9bad46ae74ef1f0aae71e7
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-11 20:27:04 +0300

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * _islower - checks for lowercase character
- * @c - character to be checked
+ * @c: character to be checked
  * Return: Always 0 (Success)
  */
 int _islower(int c)
