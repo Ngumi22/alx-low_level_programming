a596296a24596cdbb93d1bc2392c76b13ea7e5bf
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-11 23:32:39 +0300

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @i: numbers that are multiples of 3 and 5 upto 1024
+ * Return: sum of multiples of 3 and 5
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
