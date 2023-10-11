2c296f141225c2d45e9a2efc5e94ef47374e72ef
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-11 22:43:24 +0300

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 /**
  * add - adds two integers
  * @sum: result of the addition
+ * @a: one integer
+ * @b: another integer
  * Return: Sum of a and b integers
  */
 int add(int a, int b)
@@ -9,5 +11,5 @@ int add(int a, int b)
 	int sum;
 
 	sum = a + b;
-	return sum;
+	return (sum);
 }
