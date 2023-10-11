531ee94ab0e3bcc7518ef2518753f82ffa0182f2
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-11 21:35:00 +0300

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,9 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-	{
-		last_digit = -n % 10;
-		_putchar(last_digit + '0');
-	}
-	else
-	{
-		last_digit = n % 10;
-		_putchar(last_digit + '0');
-	}
+	last_digit = labs(n) % 10;
+
+	_putchar(last_digit + '0');
+
 	return (last_digit);
 }
