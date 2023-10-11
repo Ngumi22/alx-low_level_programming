09759bc1623a594d2defd6ebe1a8fa855b0076e2
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-11 19:42:52 +0300

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
- * Description: '0-putchar.c'
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * print_alphabet - Entry point
- * Description: 'Prints alphabet in lowercase followed by newline'
+ * print_alphabet: 'Prints alphabet in lowercase followed by new line'
  * Return: 0
  */
 void print_alphabet(void)
 {
-	char c;
+	char c = 'a';
 
-	for (char c = 'a'; c <= 'z'; c++)
+	while (c <= 'z')
 	{
 		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
