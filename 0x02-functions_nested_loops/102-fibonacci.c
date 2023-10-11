7a4f0098024b69dd6b38b6bda8c37e478b86c3c5
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-12 00:26:35 +0300

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: first 50 fibonacci numbers
+ */
+int main(void)
+{
+	int i;
+	unsigned long long fib[50];
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+
+	for (i = 0; i < 50; i++)
+	{
+		printf("%llu", fib[i]);
+		if (i != 49)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+	return 0;
+}
