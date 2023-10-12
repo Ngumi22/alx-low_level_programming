494ebb4e9d100c5aef11a1416d556e8cf254df77
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-12 20:38:27 +0300

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,14 +5,15 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
 	
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			_putchar("%d", j);
 		}
-		_putchar('\n');
+		_putchar("\n");
 	}
 }
