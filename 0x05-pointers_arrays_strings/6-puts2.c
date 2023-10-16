366e1f791e42cd4eaa39efc8db6dc9b6e87d601c
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-16 23:56:46 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - Prints every character of a string
+ * starting with the first character
+ * @str: String to prints characters of
+ * Return: Characters of a string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+}
