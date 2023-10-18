1b4b206e4464b0d67791d82a153b2e3bf5764708
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-18 22:03:26 +0300

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: Input value
+ * Return: Capitalized words
+ */
+
+char *cap_string(char *str)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int capitalize = 1;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		for (int j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j])
+			{
+				capitalize = 1;
+				break;
+			}
+		}
+		if (capitalize)
+		{
+			str[i] = toupper(str[i]);
+			capitalize = 0;
+		}
+		else
+		{
+			str[i] = tolower(str[i]);
+		}
+	}
+	return (str);
+}
