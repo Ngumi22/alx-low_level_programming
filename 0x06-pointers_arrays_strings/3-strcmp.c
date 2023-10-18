7eaa38c3f4b4dd90d261e70047d1c919b92ecb8c
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-18 20:52:44 +0300

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Compares two strings
+ * @s1: First input value
+ * @s2: Second input value
+ * Return: Zero Success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+
+	return (0);
+}
