1bf2e02b644d991f5bd09b280dcfbd7a06685f4f
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-23 21:30:38 +0300

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strstr - locates a substring
  * @haystack: String to locate the substring
  * @needle: Substring to locate
- * Return:  pointer to the beginning of the located
+ * Return:  pointer to the beginning of
  * substring, or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
