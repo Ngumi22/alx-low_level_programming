11249b237cc72f291733c15f0200a55352a37030
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-25 20:07:52 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,16 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: String to be printed
- * Return: String
+ * Return: Nothing
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
-	_puts_recursion(s + 1);
+	s++;
+	_puts_recursion(s);
 }
