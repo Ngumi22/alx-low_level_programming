9dacf0324e29b2669434b0c9e24797903a49b51b
Ngumi22 <mwaurapngumi@gmail.com>
2023-10-27 22:35:52 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: number of command-line arguments
+ * @argv: Array command-line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		_putchar ("%s\n", argv[0]);
+	}
+	return (0);
+}
