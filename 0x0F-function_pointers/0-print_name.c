b52bd61ad548e353979fe0ee15dc88bc7f25a1bd
Ngumi22 <mwaurapngumi@gmail.com>
2023-11-08 20:40:44 +0300

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
 #include "function_pointers.h"
 #include <stddef.h>
 /**
- * print_name - prints a name using the specified formatting function
- * @name: name of the person
+ * print_name - prints a name using pointer to a function
+ * @name: String to add
  * @f: pointer to a function for formatting the name
- *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL && f == NULL)
 	{
-		f(name);
+		return;
 	}
+	f(name);
 }
