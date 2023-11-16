8bcc8d13f295cf82396fc82beca9f8b7b2eb7628
Ngumi22 <mwaurapngumi@gmail.com>
2023-11-16 22:54:57 +0300

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,19 +3,9 @@
  * before_main - Prints message before main function
  * Return: 0
  */
-void __attribute__((constructor)) before_main(void);
-
+void before_main(void) __attribute__((constructor));
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-/**
- * before_main - Prints message before main function
- * __attribute__((constructor)) before_main(void) - Prints message before main
- * Return: 0
- */
-void __attribute__((constructor)) before_main(void)
-{
-	before_main();
 }
