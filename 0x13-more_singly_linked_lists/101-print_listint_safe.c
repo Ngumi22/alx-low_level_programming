8477854116c1f2fce2a5ee8a7474c00959e1875e
Ngumi22 <mwaurapngumi@gmail.com>
2023-11-22 10:20:24 +0300

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a listint_t linked list safely
+ * @head: pointer to the head of the list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tortoise, *hare;
+	size_t count = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	tortoise = hare = head;
+
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		count++;
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
+		{
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+			printf("Loop detected, exiting...\n");
+			exit(98);
+		}
+	}
+	return (count);
+}
