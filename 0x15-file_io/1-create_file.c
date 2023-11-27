204d62bc2821494077b622458c1514c2ae9c3711
Ngumi22 <mwaurapngumi@gmail.com>
2023-11-28 00:18:56 +0300

    1-create_file.c, 2-append_text_to_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: Pointer to a filename
+ * @text_content: content writed in the file
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDesc;
+	ssize_t bytesWritten;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fileDesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fileDesc == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		bytesWritten = write(fileDesc, text_content, strlen(text_content));
+
+		if (bytesWritten == -1)
+		{
+			close(fileDesc);
+			return (-1);
+		}
+	}
+	close(fileDesc);
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: Pointer to a filename
+ * @text_content: content writed in the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fileDescriptor;
+	ssize_t bytesWritten;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	fileDescriptor = open(filename, O_WRONLY | O_APPEND);
+
+	if (fileDescriptor == -1)
+	{
+		return (-1);
+	}
+	bytesWritten = write(fileDescriptor, text_content, strlen(text_content));
+
+	if (bytesWritten == -1)
+	{
+		close(fileDescriptor);
+		return (-1);
+	}
+	close(fileDescriptor);
+	return (1);
+}
