13491b946abd997f7fbb9db90491a5125f05808b
Ngumi22 <mwaurapngumi@gmail.com>
2023-11-28 00:03:49 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,39 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints
- * it to the POSIX standard output
- * @filename: Pointer to the file
- * @letters: number of letters it should read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: Pointer to filename
+ * @letters: Numbers of letters printed
  * Return: number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	ssize_t bytesRead, bytesWritten;
+	int fileDescriptor;
+	ssize_t nrd, nwr;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
+		return (0);
+
+	fileDescriptor = open(filename, O_RDONLY);
+
+	if (fileDescriptor == -1)
 	{
-		fprintf(stderr, "Error: filename is NULL\n");
 		return (0);
 	}
-	file = fopen(filename, "r");
-	if (file == NULL)
+
+	buffer = malloc(sizeof(char) * (letters));
+	if (!buffer)
 	{
-		fprintf(stderr, "Error: Couldn't open file %s\n", filename);
 		return (0);
 	}
-	buffer = (char *)malloc(letters + 1);
-	if (buffer == NULL)
-	{
-		fprintf(stderr, "Error: Memory allocation failed\n");
-		fclose(file);
-		return (0);
-	}
-	bytesRead = fread(buffer, 1, letters, file);
-	fclose(file);
-	if (bytesRead == 0)
-	{
-		fprintf(stderr, "Error: Couldn't read from file\n");
-		free(buffer);
-		return (0);
-	}
-	buffer[bytesRead] = '\0';
-	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+
+	nrd = read(fileDescriptor, buffer, letters);
+	nwr = write(STDOUT_FILENO, buffer, nrd);
+
+	close(fileDescriptor);
+
 	free(buffer);
 
-	if (bytesWritten != bytesRead)
-	{
-		fprintf(stderr, "Write failed or did not write expected amount of bytes\n");
-		return (0);
-	}
-	return (bytesRead);
+	return (nwr);
 }
